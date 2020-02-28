2d119b301378821c2bc5d14067edaaa4d8589903
Balint <abalint@techteam.com>
2020-02-28 20:16:26 +0200

    Recalculate A's score
    If A is already calculated as 11 and with a new card the total score is > 21, A is recalculated as 1

--- static/js/game.js
+++ static/js/game.js
@@ -1,21 +1,6 @@
-function ageInDays(){
-var birthYear = prompt('What year were you born?');
-var ageInDayss = (2020 - birthYear) * 365;
-var h1 = document.createElement('h1');
-var textAnswer = document.createTextNode('You are ' + ageInDayss + ' days old.');
-h1.setAttribute('id', 'ageInDays');
-h1.appendChild(textAnswer);
-document.getElementById('flex-box-result').appendChild(h1);
-}
-
-function reset(){
-    document.getElementById('ageInDays').remove();
-}
-// Blackjack challange
-
 let blackjackGame = {
-    'you': {'scoreSpan': '#your-blackjack-result', 'div': '#your-box','score': 0},
-    'dealer': {'scoreSpan': '#dealer-blackjack-result', 'div': '#dealer-box', 'score': 0},
+    'you': {'scoreSpan': '#your-blackjack-result', 'div': '#your-box','score': 0, 'currentCards': []},
+    'dealer': {'scoreSpan': '#dealer-blackjack-result', 'div': '#dealer-box', 'score': 0, 'currentCards': []},
     'cards': ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'],
     'cardsMap': {'2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9, '10': 10, 'J': 10, 'Q': 10, 'K': 10, 'A': [1, 11]},
     'whoWon' : {'player1': 'YOU WON', 'player2': 'DEALER WON', 'tie': 'DRAW'},
@@ -29,18 +14,13 @@ document.querySelector('#blackjack-hit-button').addEventListener('click', blackj
 document.querySelector('#blackjack-stand-button').addEventListener('click', dealerLogic);
 document.querySelector('#blackjack-deal-button').addEventListener('click', blackjackDeal);
 
-function blackjackHit(){
-    let card = randomCard();
-    showCard(card, YOU);
-    updateScore(card, YOU);
-    showScore(YOU);
-}
-
+// GENERATE A RANDOM CARD
 function randomCard(){
     let randomIndex = Math.floor(Math.random() * 13);
     return blackjackGame['cards'][randomIndex];
 }
 
+// SHOW THE CARD IN THE CURRENT PLAYER DIV
 function showCard(card, activePlayer){
     if (activePlayer['score'] <= 21){
         let cardImage = document.createElement('img');
@@ -49,37 +29,59 @@ function showCard(card, activePlayer){
         hitSound.play();
     }
 }
-    
-function blackjackDeal() {
-    let yourImages = document.querySelector('#your-box').querySelectorAll('img');
-    for (i=0; i<yourImages.length; i++){
-        yourImages[i].remove();
-    }
-    let dealerImages = document.querySelector('#dealer-box').querySelectorAll('img');
-    for (i=0; i<dealerImages.length; i++){
-        dealerImages[i].remove();
-    }
-    YOU['score'] = 0;
-    DEALER['score'] = 0;
 
-    document.querySelector('#your-blackjack-result').textContent = 0;
-    document.querySelector(['#your-blackjack-result']).style.color = 'white';
-    document.querySelector('#dealer-blackjack-result').textContent = 0;
-    document.querySelector(['#dealer-blackjack-result']).style.color = 'white';
+// DEALER LOGIC HERE
+
+function dealerLogic(){
+    let card = randomCard();
+
+    showCard(card, DEALER);
+    updateScore(card, DEALER);
+    showScore(DEALER);
+    blackjackGame['dealer']['currentCards'].push(card);
+    console.log(blackjackGame['dealer']['currentCards']);
+
+    if (DEALER['score'] < YOU['score'] && YOU['score'] <= 21){
+        dealerLogic();
+    } else {
+        computeWinner();
+    }
 }
+
+//PLAYER LOGIC HERE
+
+function blackjackHit(){
+    let card = randomCard();
+    showCard(card, YOU);
+    updateScore(card, YOU);
+    blackjackGame['you']['currentCards'].push(card);
+    console.log(blackjackGame['you']['currentCards']);
+    showScore(YOU);
+}
+
+//UPDATE THE CURRENT SCORE BASED ON THE DREW CARD
 
 function updateScore(card, activePlayer){
     if (card === 'A'){
         if (activePlayer['score'] + blackjackGame['cardsMap'][card][1] <= 21) {
             activePlayer['score'] += blackjackGame['cardsMap'][card][1];
+            activePlayer['hasA'] = true;
         } else {
-            activePlayer['score'] += blackjackGame['cardsMap'][0];
-                }
+            activePlayer['score'] += blackjackGame['cardsMap'][card][0];
+        }
     } else {
+        for (var i=0; i < activePlayer['currentCards'].length; i++){
+            if (activePlayer['currentCards'][i] == 'A' && activePlayer['score'] + blackjackGame['cardsMap'][card] > 21){
+                activePlayer['score'] = activePlayer['score'] - 10;
+            } else {
+
+            }
+        } 
         activePlayer['score'] += blackjackGame['cardsMap'][card];
     }
-    
 }
+
+//SHOW THE SCORE
 
 function showScore(activePlayer) {
     if (activePlayer['score'] > 21) {
@@ -90,26 +92,13 @@ function showScore(activePlayer) {
     }
 }
 
-function dealerLogic(){
-    let card = randomCard();
-
-    showCard(card, DEALER);
-    updateScore(card, DEALER);
-    showScore(DEALER);
-
-    if (DEALER['score'] < YOU['score'] && YOU['score'] <= 21){
-        dealerLogic();
-    } else {
-        computeWinner();
-    }
-}
+//CALCULATE AND SEE WHO IS THE WINNER
 
 function computeWinner(){
     let winner;
     let currentWins = document.querySelector('#wins').innerHTML;
     let currentLosses = document.querySelector('#losses').innerHTML;
     let currentDrews = document.querySelector('#Draws').innerHTML;
-
 
     if (YOU['score'] <= 21) {
         if (YOU['score'] > DEALER['score'] || (DEALER['score'] >21)){
@@ -137,4 +126,27 @@ function computeWinner(){
     console.log(winner);
     document.querySelector('#blackjack-result').textContent = winner;
     return winner;
+}
+
+//START A NEW GAME
+
+function blackjackDeal() {
+    let yourImages = document.querySelector('#your-box').querySelectorAll('img');
+    for (i=0; i<yourImages.length; i++){
+        yourImages[i].remove();
+    }
+    let dealerImages = document.querySelector('#dealer-box').querySelectorAll('img');
+    for (i=0; i<dealerImages.length; i++){
+        dealerImages[i].remove();
+    }
+    YOU['score'] = 0;
+    DEALER['score'] = 0;
+
+    document.querySelector('#your-blackjack-result').textContent = 0;
+    document.querySelector(['#your-blackjack-result']).style.color = 'white';
+    document.querySelector('#dealer-blackjack-result').textContent = 0;
+    document.querySelector(['#dealer-blackjack-result']).style.color = 'white';
+    blackjackGame['you']['currentCards'] = [];
+    blackjackGame['dealer']['currentCards'] = [];
+    blackjackHit();
 }
